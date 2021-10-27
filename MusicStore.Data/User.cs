0fa430a18069af507529fd7fae8ecc8e5e07beaf
artyom <vegitoxxxgogeta@gmail.com>
2021-10-27 18:25:05 +0300

    Created user model

---
+++ MusicStore.Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Data
+{
+    public class User
+    {
+        [Key] public int ID { get; set; }
+        [DataType(DataType.Text)] public string Password { get; set; }
+        [DataType(DataType.EmailAddress)] public string Email { get; set; }
+        [DataType(DataType.Text)] public string Name { get; set; }
+        
+    }
+}
